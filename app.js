c11d8248289eff22becf3c26999610b76aeda207
Diogo Oliveira <diogooliveira71@hotmail.com>
2025-01-16 15:27:07 -0300

    Alterando o limite de numeros sorteados para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag,texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do numero Secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 50');
 }
 
 function verificarChute() {
